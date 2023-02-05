cd79310f3c8ce7a0fb0f4b88c2a39533c39a75ff
steev <mauricefl@outlook.de>
2023-02-05 01:24:07 +0100

    added script abillity for UI

---
+++ render.js
@@ -0,0 +1,28 @@
+function updateProgressBar(progress){
+    document.getElementById("progress-bar").style.width = progress + "%";
+    document.getElementById("progress-label").textContent = progress + "%";
+}
+
+function updateProgressStep(step){
+    document.getElementById("progress-step").textContent = step;
+}
+
+function toggleDownloadButton(shown){
+    let button = document.getElementById("download");
+
+    if (shown) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function toggleDownloadDisplay(shown){
+
+    if (shown) {
+        document.getElementById("download-section").style.display = "block";
+    } else {
+        document.getElementById("download-section").style.display = "none";
+    }
+}
+
